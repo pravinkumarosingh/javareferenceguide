840fb0b85f8c1403ae71d6676a2e0e804509ac8e
pravinkumarosingh <headfryer@gmail.com>
2022-11-29 21:54:51 +0530

    reduction operation example.

---
+++ part_02/chapter_29_stream/Example2.java
@@ -0,0 +1,18 @@
+package part_02.chapter_29_stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Example2 {
+    public static void main(String[] args) {
+        List<Integer> numberList = List.of(7,18,10,24,17,5);
+        List<Integer> myList = new ArrayList<>(numberList);
+
+        Optional<Integer> productObj = myList.stream().reduce((a,b) -> a*b);
+        System.out.println("Product as optional "+productObj.get());
+
+        int product = myList.stream().reduce(1,(a,b)->a*b);
+        System.out.println("Product as int "+product);
+    }
+}
